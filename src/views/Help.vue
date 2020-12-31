36573825239eb4127deef45af1704cc71882c134
Basil <basil@summation.app>
2020-12-31 14:22:25 -0800

    help

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,5 @@
 <template>
-	<iframe src="https://summation.freshdesk.com/support/solutions" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+	<iframe src="https://onepress.developerhub.io/onepress/methods" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
 </template>
 
 <script>
