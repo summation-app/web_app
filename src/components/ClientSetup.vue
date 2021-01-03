43fa4891521e9b0bc1c3bb589744c467f789e0cd
Basil <basil@summation.app>
2021-01-03 14:43:59 -0800

    vue syntax highlighting for code blocks

--- src/components/ClientSetup.vue
+++ src/components/ClientSetup.vue
@@ -9,6 +9,7 @@
 		</v-text-field>
 		<v-btn color="primary" @click="generate_gateway_keys()" :loading="pending_submit">Generate Keys</v-btn>
 		<div v-if="show_instructions">
+			<div>Please select a language to see the setup instructions:</div>
 			<v-tabs
 			v-model="selected_tab"
 			grow
@@ -16,10 +17,75 @@
 			<v-tab key='web'>
 				Web (Javascript)
 			</v-tab>
+			<v-tab key='ios'>
+				iOS (Swift)
+			</v-tab>
+			<v-tab key='android'>
+				Android (Kotlin)
+			</v-tab>
 			</v-tabs>
 			<v-tabs-items v-model="selected_tab">
 			<v-tab-item key='web'>
-				<iframe src='https://docs.summation.app/web-client-api/initialization' style='width: 100%'></iframe>
+				<ol>
+					<li>Install the node package by running this command from your terminal:
+					<vue-code-highlight language="javascript">
+					<pre>
+					npm install --save summation_web_client
+					</pre>
+					</vue-code-highlight>
+					</li>
+					<li>Add this import to your JavaScript code:
+					<vue-code-highlight language="javascript">
+					<pre>
+					import Summation from 'summation_web_client';
+					</pre>
+					</vue-code-highlight>
+					</li>
+					<li>Initialize the gateway:
+					<vue-code-highlight language="javascript">
+					<pre>
+					const gw = new Summation({
+					gateway_url: YOUR_GATEWAY_URL,
+					token: USER_TOKEN,
+					gateway_token: GATEWAY_TOKEN,
+					default_database: "YOUR_DATABASE_NAME"
+					});
+					</pre>
+					</vue-code-highlight>
+					</li>
+					<li>Get your data by running a query:
+					<vue-code-highlight language="javascript">
+					<pre>
+					results = gw.query("SELECT * FROM table WHERE id=:id", {id: 4}, "prod"))
+					</pre>
+					</vue-code-highlight>
+					<li>
+				</ol>
+			</v-tab-item>
+			<v-tab-item key='ios'>
+				<ol>
+					<li>Cocoapods: pod 'Summation-iOS-Client'</li>
+					<li>import Summation_iOS_Client</li>
+					<li>let options = SummationOptions(gatewayUrl: gatewayUrl, token: token,
+                                       gatewayToken: gateway_token, defaultDatabase: defaultDatabase)
+        summationClient.setOptions(options)
+        _ = summationClient.db(.query(sql: "SELECT * FROM queries WHERE id=:id", parameters: ["id": 1], databaseName: defaultDatabase))</li>
+				</ol>
+			</v-tab-item>
+			<v-tab-item key='android'>
+				<ol>
+					<li>Gradle: implementation("app.summation.android:android-client:1.0")</li>
+					<li>val client = Client(
+						gatewayUrl = "<your gateway url>",
+						token = "<your token>",
+						gatewayToken = "<your gateway token>",
+						defaultDatabase = "<your database name>",
+					)
+					val params = HashMap<String, Any>()
+					params.put("id", 1)
+					val response = client.query("SELECT * FROM queries WHERE id=:id", params)
+					println(response)</li>
+				</ol>
 			</v-tab-item>
 			</v-tabs-items>
 		</div>
@@ -28,6 +94,7 @@
 
 <script>
 const axios = require('axios').default;
+import { component as VueCodeHighlight } from 'vue-code-highlight';
 
 export default {
 	props: ['user','token','gw'],
@@ -69,7 +136,7 @@ export default {
 	  }
 	},
 	components: {
-
+		VueCodeHighlight,
 	},
 	async created() 
 	{
